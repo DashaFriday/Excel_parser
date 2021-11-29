1325e24b16d1e6ba6472f9f9fe97c7a2222c63d2
”DashaFriday” <watch21angel@gmail.com>
2021-11-29 17:57:58 +0300

    Code version 2.0 + input from console + exception processor

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 import openpyxl as op
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import pandas as pd
 
 
 # выбор всех уникальных строк из файла по указанному столбцу
@@ -34,33 +36,84 @@ def print_info(count, sum, arr_of_values):
     print(text[2])
     text.append('Percentile 99: ' + str(round(np.percentile(arr_of_values, 99), 2)))
     print(text[3])
+    text.append('Max: ' + str(max(arr_of_values)))
+    print(text[4])
     return text
 
 
-# буквы нужных столбцов (может позже сделать ввод пользователя)
-col_elapse = 'A'
-col_t_s = 'B'
-col_label = 'C'
-start_label = 'C2'
+# удаление слеша
+def del_slashes(checked_string):
+    verified_string = ''
+    if checked_string.find('/') != -1:
+        verified_string = checked_string.replace('/', '-')
+    else:
+        verified_string = checked_string
+    return verified_string
 
-# выбор файла эксель (может позже сделать ввод пользователя)
-# выбор активного листа
-# ВАЖНО
-# подразумевается, что файл может быть не отсортирован по именам, но должен быть отсортирован по времени
-# также в файле стоит убрать / в именах
-my_file = op.load_workbook('Agreggare_23.11_night.xlsx')
-sheet = my_file.active
 
-# получаем счётчик и массив с уникальными лейблами, сортируем массив уникальных имён (?)
-main_array, counter_of_lines = unique_label(my_file.active, col_label)
+# временный файл для сортировки
+temp_file = 'file.xlsx'
 
-main_array.sort()
+# блок ввода имени файла + проверка
+name_of_file = input('Введите путь к файлу и имя файла: ')
+print('Проверка файла...')
+
+try:
+    source_file = op.load_workbook(name_of_file)
+except op.utils.exceptions.InvalidFileException:
+    print('Не верное имя или формат файла. Поддерживаются файлы с расширениями .xlsx,.xlsm,.xltx,.xltm')
+    exit()
+except FileNotFoundError:
+    print('Файл с указанным именем не найден')
+    exit()
+else:
+    print('OK')
+
+# блок ввода имени листа + проверка
+name_of_sheet = input('Введите название листа: ')
+
+try:
+    sheet = source_file[name_of_sheet]
+except KeyError:
+    print('Листа с данным названием не существует')
+    exit()
+else:
+    print('OK')
+
+# блок ввода букв столбцов
+col_elapse = input('Введите букву столбца с временем отклика: ')
+col_t_s = input('Введите букву столбца с временными метками: ')
+col_label = input('Введите букву столбца с лейблами: ')
+start_label = col_label + str(2)
+name_of_col_sort = sheet[col_t_s + str(1)].value
+
+# сортировка файла по таймстемпам
+source_file = pd.ExcelFile(name_of_file)
+sheet = source_file.parse(name_of_sheet)
+
+# проверка правильности при вводе буквы столбца временных меток
+# при удаче запись отсортированных данных в файл-копию
+try:
+    sheet = sheet.sort_values(by=name_of_col_sort)
+    sheet.to_excel(temp_file, sheet_name=name_of_sheet, index=False)
+except KeyError:
+    print('Ошибка данных в столбце временных меток')
+    exit()
+
+# окончание работы с исходным файлом
+source_file.close()
+
+# начало работы с копией данных
+my_file = op.load_workbook(temp_file)
+sheet = my_file[name_of_sheet]
+
+# получаем счётчик и массив с уникальными лейблами
+main_array, counter_of_lines = unique_label(sheet, col_label)
 
 # прогон кода для каждого лейбла
 all_names = map(str, main_array)
 for name in all_names:
 
-    print(name)
     pass_counter = 0
     summa = 0
     arr_of_elapse = []
@@ -76,20 +129,26 @@ for name in all_names:
         ts = col_t_s + str(k)
 
         # если имя совпадает с искомым добавляем счётчик, плюсуем сумму
+        # проверяем правильность ввода для столбца с временами отклика
         if str(sheet[l].value) == str(name):
-            pass_counter += 1
-            summa += int(str(sheet[e].value))
+            try:
+                pass_counter += 1
+                summa += int(str(sheet[e].value))
 
-            # переводим таймстемп в человекочитаемый вид
-            timestamp = int(str(sheet[ts].value)) / 1000
-            time_format = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(timestamp))[11:]
+                # переводим таймстемп в человекочитаемый вид
+                timestamp = int(str(sheet[ts].value)) / 1000
+                time_format = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(timestamp))[11:]
 
-            # добавляем полученные значения в массивы
-            arr_of_elapse.append(sheet[e].value)
-            arr_of_ts.append(time_format)
+                # добавляем полученные значения в массивы
+                arr_of_elapse.append(sheet[e].value)
+                arr_of_ts.append(time_format)
+            except ValueError:
+                print('Ошибка данных в столбце времени отклика')
+                exit()
 
+    print(name)
 
-    # в массив выводим итоговую информацию по транзакции
+    # в массив выводим итоговую информацию по транзакции + вывод в консоль
     main_array = print_info(pass_counter, summa, arr_of_elapse)
 
     # настраиваем вид графика, данные и тд
@@ -98,7 +157,7 @@ for name in all_names:
     plt.figure(figsize=(12, 6), facecolor='aliceblue')
 
     # для графика точками plt.scatter
-    plt.plot(arr_of_ts, arr_of_elapse, label ='Время отклика')
+    plt.plot(arr_of_ts, arr_of_elapse, label='Время отклика')
 
     plt.suptitle(name)
     plt.subplots_adjust(left=0.06, bottom=0.15, top=0.94, right=0.8)
@@ -109,13 +168,16 @@ for name in all_names:
     plt.figtext(0.83, 0.7, main_array[1])
     plt.figtext(0.83, 0.6, main_array[2])
     plt.figtext(0.83, 0.5, main_array[3])
+    plt.figtext(0.83, 0.4, main_array[4])
 
     # вывод графика в окно
     plt.show()
 
-    # # сохранение графика в папку проекта и закрытие графика
-    # way = str(name) + '.png'
-    # plt.savefig(way, bbox_inches = 'tight')
+    # сохранение графика в папку проекта и закрытие графика
+    # way = del_slashes(name) + '.png'
+    # plt.savefig(way, bbox_inches='tight')
     # plt.close()
 
+# закрываем и удаляем временный файл
 my_file.close()
+os.remove(temp_file)
